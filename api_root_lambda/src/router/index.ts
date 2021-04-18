7e31b778ff9eb30880375800d11db70d25dcf1c7
Xuejin Gao <xuejingao@gmail.com>
2021-04-18 16:29:09 -0400

    Added router for dogapi

--- api_root_lambda/src/router/index.ts
+++ api_root_lambda/src/router/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import echoRouter from './echo';
+import dogRouter from './dog';
 
 const router = Router();
 
 router.use('/echo', echoRouter);
+router.use('/dogfact', dogRouter);
 
 router.route('/').get((_, res) => {
   res.status(200).json({ message: 'Hello World' });
---
+++ api_root_lambda/src/util/randomInt.ts
@@ -0,0 +1,5 @@
+const randomInt = (n: number) : number => {
+  return Math.floor(Math.random() * n);
+};
+
+export default randomInt;
